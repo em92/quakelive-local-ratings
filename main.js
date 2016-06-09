670cfdfd017a7f299a0008fc10f24bb0625b5c41
Eugene M <eugene.m92@gmail.com>
2016-06-09 14:49:15 +0500

    :trollface: :trollface: :trollface:

--- main.js
+++ main.js
@@ -1,51 +1,59 @@
 var express = require('express');
 var rating = require('./rating.js');
 
-var LISTEN_PORT = 6480;
+// ToDo: read from cfg file
+var LISTEN_PORT = 7082;
 
 var app = express();
 
 
 var get_int_from_string = function(s, default_value) {
-	
-	if (typeof(s) == 'undefined')
-		return default_value;
-	
-	var result = parseInt(s);
-	if (isNan(result))
-		return default_value;
-	
-	return result;
+
+  if (typeof(s) == 'undefined')
+    return default_value;
+
+  var result = parseInt(s);
+  if (isNan(result))
+    return default_value;
+
+  return result;
 };
 
 
 app.get(["/elo/:ids", "/elo_b/:ids"], function(req, res) {
-	
-	var ids = req.params.ids.split("+");
-	
-	rating.getForSteamIds(ids, function(result) {
-		res.setHeader("Content-Type", "application/json");
-		res.send(result);
-	});
+
+  var ids = req.params.ids.split("+");
+
+  rating.getForSteamIds(ids, function(result) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(result);
+  });
 });
 
 
 app.get(["/rating/:gametype", "/rating/:gametype/:page"], function(req, res) {
-	
-	var page = get_int_from_string(req.params.page, 0);
-	
-	rating.getList(req.params.gametype, page, function(result) {
-		res.setHeader("Content-Type", "application/json");
-		res.send(result);
-	});
 
+  var page = get_int_from_string(req.params.page, 0);
+
+  rating.getList(req.params.gametype, page, function(result) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(result);
+  });
 });
 
+
+app.post("/stats/submit", function(req, res) {
+  // ToDo: добавить проверку заголовка (посмотри у фидера)
+  // ToDo: добавить результаты матча
+  // ToDo: выдернуть имена и засунуть в коллекцию players
+  // ToDo: пересчитать 
+  // ToDo: в это время считают рейтинг, то добавить в очередь для считывания
+  res.setHeader("Content-Type", "application/json");
+  res.send({ok: false});
+});
 
 app.use(express.static("public"));
 
 app.listen(LISTEN_PORT, function () {
-	console.log("Listening on port " + LISTEN_PORT.toString());
+  console.log("Listening on port " + LISTEN_PORT.toString());
 });
-
-rating.getList("tdm", 0, console.log);

880e92eafdfc2788216fa768553d310cb2b6a850
Eugene Molotov <eugene.m92@gmail.com>
2016-07-08 13:51:05 +0500

    added run_post_process param to cfg and allowed to read external config file

--- cfg.js
+++ cfg.js
@@ -6,10 +6,13 @@ var default_cfg = {
   "db_url": "mongodb://localhost:27017/quakelive-local-ratings",
   "player_count_per_page": 10,
   "httpd_port": 7081,
+  "run_post_process": true,
   "moving_average_count": 50
 };
 var allowed_param_names = Object.keys(default_cfg);
 var cfg = {};
+var argv = process.argv.slice(2);
+var cfg_filename = argv[0] ? argv[0] : "./cfg.json";
 
 var load = function() {
   
@@ -30,12 +33,21 @@ var load = function() {
     }
   };
   
+  var use_default_if_not_bool = function( param_name ) {
+    try {
+      if (typeof(cfg[ param_name ]) == "undefined") throw new Error(param_name + " is undefined");
+      if (typeof(cfg[ param_name ]) != "boolean")   throw new Error(param_name + " is not boolean");
+    } catch (e) {
+      use_default( e.message, param_name );
+    }
+  };
+  
   // reading and parsing from file
   try {
-    var filedata = fs.readFileSync(path.resolve(__dirname, 'cfg.json'), 'UTF-8');
+    var filedata = fs.readFileSync(cfg_filename, 'UTF-8');
     cfg = JSON.parse( filedata );
   } catch(e) {
-    console.log("Coudn't load cfg.json. Reason: " + e.message);
+    console.log("Coudn't load " +  cfg_filename + ". Reason: " + e.message);
     console.log("Using default cfg...");
     cfg = default_cfg;
   }
@@ -60,12 +72,13 @@ var load = function() {
         throw new Error("invalid path: " + url_data.path);
     } else use_default( "db_url is undefined", 'db_url' );
   } catch(e) {
-    use_default( e.message, 'db_url' )
+    use_default( e.message, 'db_url' );
   }
   
   use_default_if_not_uint( 'httpd_port' );
   use_default_if_not_uint( 'player_count_per_page' );
   use_default_if_not_uint( 'moving_average_count' );
+  use_default_if_not_bool( 'run_post_process' );
 };
 
 try {

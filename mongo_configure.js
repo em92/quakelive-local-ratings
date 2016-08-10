373c0174b2db34afaff05fda2915048723d293ff
Eugene Molotov <eugene.m92@gmail.com>
2016-08-10 10:44:16 +0500

    removed nodejs files

--- mongo_configure.js
+++
@@ -1,8 +0,0 @@
-var cfg = cat('cfg.json');
-cfg = JSON.parse(cfg);
-var url = cfg["db_url"].replace("mongodb://", "");
-db = connect(url);
-db.createCollection("matches");
-db.createCollection("players");
-db.createCollection("scoreboards");
-quit()

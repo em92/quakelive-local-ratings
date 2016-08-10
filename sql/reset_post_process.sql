34f8883db4bf8296fe955e48222efc98ef4f0aa0
Eugene Molotov <eugene.m92@gmail.com>
2016-08-10 10:21:32 +0500

    disabled cheater to show in top list

--- sql/reset_post_process.sql
+++ sql/reset_post_process.sql
@@ -2,3 +2,4 @@ UPDATE scoreboards SET history_rating = NULL;
 UPDATE matches SET post_processed = FALSE;
 UPDATE gametype_ratings SET rating = NULL, n = 0;
 
+UPDATE gametype_ratings SET n = -999 WHERE steam_id = 76561198054367317;

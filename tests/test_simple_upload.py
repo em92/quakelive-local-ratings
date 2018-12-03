a5f52bc61266ec500762eea9f598d28eb6eaf622
Eugene Molotov <eugene.molotov@yandex.ru>
2018-12-03 12:30:00 +0500

    Adds test_upload_sample_again to make sure, that one cannot be uploaded multiple times

--- tests/test_simple_upload.py
+++ tests/test_simple_upload.py
@@ -10,16 +10,17 @@ class TestSimpleUpload(AppTestCase):
         self.assertTrue("signature" in json['message'].lower())
 
 # TODO:
-# match already exists
 # shit posting examples
 # 202 or 200
 
-# TODO in next major release:
-# no 500 errors
     def test_upload_junk(self):
         resp = self.upload_match_report(sample="junk sample")
-        self.assertEqual(resp.status_code, 500)
+        self.assertEqual(resp.status_code, 500)  # TODO: change to 422 in future
 
     def test_upload_sample(self):
         resp = self.upload_match_report(sample_name="sample01")
         self.assertEqual(resp.status_code, 200)
+
+    def test_upload_sample_again(self):
+        resp = self.upload_match_report(sample_name="sample01")
+        self.assertEqual(resp.status_code, 422)  # TODO: change to 409 in future

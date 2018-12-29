7d9fb2a5db74159d9dc1c380c8e49bd48361adc9
Eugene Molotov <em92@users.noreply.github.com>
2018-12-29 10:37:42 +0500

    returned back testing.postgresql usage

--- tests/fixture.py
+++ tests/fixture.py
@@ -2,9 +2,31 @@ import gzip
 import os
 import sys
 import unittest
+import psycopg2
+
+from testing import postgresql as pgsql_test
 from conf import settings
 
-settings['db_url'] = "postgres://eugene:bebebe@db:5432/qllr"
+def handler(postgresql):
+    f = open(os.path.dirname(os.path.realpath(__file__)) + "/../sql/init.sql")
+    sql_query = f.read()
+    f.close()
+    conn = psycopg2.connect(**postgresql.dsn())
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+
+PGSQLFactory = pgsql_test.PostgresqlFactory(
+    cache_initialized_db=True,
+    on_initialized=handler
+)
+
+postgresql = PGSQLFactory()
+os.environ["DATABASE_URL"] = postgresql.url()
+
 sys.path.append(sys.path[0] + "/..")
 from main import app  # noqa: E402
 

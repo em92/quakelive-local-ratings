3efb5f393edb8bec9db743c763aeb12d3127064e
Eugene Molotov <em92@users.noreply.github.com>
2019-02-13 11:10:17 +0500

    Add type annotations to app module

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 
 from starlette.applications import Starlette
 from starlette.exceptions import HTTPException
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 
-async def http_exception_handler(request, e):
+async def http_exception_handler(request: Request, e: HTTPException):
     return JSONResponse({'ok': False, 'message': e.detail}, status_code=e.status_code)
 
 
-async def unhandled_exception_handler(request, e):
+async def unhandled_exception_handler(request: Request, e: Exception):
     return JSONResponse({'ok': False, 'message': type(e).__name__ + ": " + str(e)}, status_code=500)
 
 
@@ -18,4 +19,4 @@ class App(Starlette):
         super().__init__()
         self.debug = True
         self.add_exception_handler(HTTPException, http_exception_handler)
-        self.add_exception_handler(500, unhandled_exception_handler)
+        self.add_exception_handler(Exception, unhandled_exception_handler)

526ffd0ad752d97d934e9f3072d025fb3b9bf297
Eugene Molotov <em92@users.noreply.github.com>
2019-02-23 23:02:57 +0500

    Implements TestScoreboard for scoreboard testing

--- tests/test_more_samples_upload.py
+++ tests/test_more_samples_upload.py
@@ -6,9 +6,7 @@ class TestMatchUpload2(AppTestCase):
     def test_upload_sample2(self):
         # TODO: make sure, sample01 is uploaded
         self.upload_match_report_and_assert_success("sample02", "44c479b9-fdbd-4674-b5bd-a56ef124e48c")
-        self.assert_scoreboard_equals_sample("44c479b9-fdbd-4674-b5bd-a56ef124e48c", "scoreboard_after_sample02.json")
 
     def test_upload_sample3(self):
         # TODO: make sure, sample02 is uploaded
         self.upload_match_report_and_assert_success("sample03", "abdf7e7d-4e79-4f1c-9f28-6c87728ff2d4")
-        self.assert_scoreboard_equals_sample("abdf7e7d-4e79-4f1c-9f28-6c87728ff2d4", "scoreboard_after_sample03.json")

dd52bc2fa3b3050d7c544d4c1d59c31886fa0ddf
Eugene Molotov <em92@users.noreply.github.com>
2019-03-02 03:16:03 +0500

    Implements tests for player module

--- tests/test_player.py
+++ tests/test_player.py
@@ -23,5 +23,35 @@ class TestPlayer(AppTestCase):
             self.upload_match_report_and_assert_success(sample_name, match_id)
 
     def test_player_json(self):
-        resp = self.test_cli.get("/player/76561198166128882.json")
-        print(resp.text)
+        steam_ids = [
+            "76561198043212328",  # shire
+            "76561198257384619",  # HanzoHasashiSan
+            "76561197985202252",  # carecry
+            "76561198308265738",  # lookaround
+            "76561198257327401",  # Jalepeno
+            "76561198005116094",  # Xaero
+            "76561198077231066",  # Mike_Litoris
+            "76561198346199541",  # Zigurun
+            "76561198257338624",  # indie
+            "76561198260599288",  # eugene
+        ]
+        for steam_id in steam_ids:
+            resp = self.test_cli.get("/player/{0}.json".format(steam_id))
+            self.assertEqual(resp.status_code, 200)
+
+            obj_defacto = resp.json()
+            obj_expected = self.read_json_sample("player_{}".format(steam_id))
+            self.assertDictEqual(obj_defacto, obj_expected)
+
+            resp = self.test_cli.get("/player/{0}".format(steam_id))
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.template.name, "player_stats.html")
+            context = resp.context
+            self.assertIn('request', context)
+            self.assertIn('steam_id', context)
+            self.assertEqual(context['steam_id'], steam_id)
+
+            del context['request']
+            del context['steam_id']
+            obj_defacto = context
+            self.assertDictEqual(obj_defacto, obj_expected)

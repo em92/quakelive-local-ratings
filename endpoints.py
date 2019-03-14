a0101a04d55d1684acdbba1e39714554c32a8513
Eugene Molotov <em92@users.noreply.github.com>
2019-03-14 17:51:18 +0500

    Writes more Endpoints code

--- endpoints.py
+++ endpoints.py
@@ -2,8 +2,10 @@
 
 from time import gmtime
 from email.utils import parsedate
-from db import cache
+from db import cache, get_db_pool
+from typing import Tuple, Optional
 
+from asyncpg import Connection
 from starlette.endpoints import HTTPEndpoint
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
@@ -14,19 +16,13 @@ from starlette.types import Message, Receive, Scope, Send
 class Endpoint(HTTPEndpoint):
     last_req_time = None
 
-    def fast_response_if_not_modified(self, request: Request):
-        if "if-modified-since" not in request.headers:
-            return
-
-        last_req_time = parsedate(request.headers["if-modified-since"])
-        if last_req_time is None:
-            return
-        self.last_req_time = last_req_time
+    def try_very_fast_response(self, request: Request):
+        last_req_time = self.last_req_time
 
         if 'gametype' in request.path_params:
-            # TODO: keyerror handling
             last_modified = cache.LAST_GAME_TIMESTAMPS[request.path_params["gametype"]]
         else:
+            # TODO: move it to cache.LAST_GAME_TIMESTAMP
             last_modified = max(cache.LAST_GAME_TIMESTAMPS.values())
 
         last_modified = gmtime(last_modified)[:]
@@ -34,8 +30,47 @@ class Endpoint(HTTPEndpoint):
         if last_req_time >= last_modified:
             raise HTTPException(304)
 
-    async def __call__(self, receive: Receive, send: Send) -> None:
-        request = Request(self.scope, receive=receive)
-        self.fast_response_if_not_modified(request)
-        response = await self.dispatch(request)
-        await response(receive, send)
+    async def try_fast_response(self, request: Request, conn: Connection) -> None:
+        last_req_time = self.last_req_time
+
+        last_modified = await self.get_last_modified(request, conn)
+        if last_modified is None:
+            return
+
+        if last_req_time >= last_modified:
+            raise HTTPException(304)
+
+    async def get(self, request: Request) -> Response:
+        # check for valid gametype
+        if request.path_params and 'gametype' in request.path_params:
+            request.path_params['gametype'] = request.path_params['gametype'].lower()
+            if request.path_params['gametype'] not in cache.GAMETYPE_IDS:
+                raise HTTPException(404, "invalid gametype: {}".format(request.path_params['gametype']))
+
+        last_req_time = parsedate(request.headers["if-modified-since"])
+        if last_req_time is not None:
+            self.last_req_time = last_req_time
+
+            self.try_very_fast_response(request)
+
+            dbpool = await get_db_pool()
+            con = await dbpool.acquire()
+            with con.transaction():
+                await self.try_fast_response(request, con)
+                return await self.get_common_response(request, con)
+        else:
+            dbpool = await get_db_pool()
+            con = await dbpool.acquire()
+            with con.transaction():
+                return await self.get_common_response(request, con)
+
+    async def get_last_modified(self, request: Request, conn: Connection) -> Optional[Tuple]:
+        return None
+
+    async def get_common_response(self, request: Request, conn: Connection) -> Response:
+        resp = await self._get(request, conn)
+        # TODO: use get_last_modified
+        return resp
+
+    async def _get(self, request: Request, conn: Connection) -> Response:
+        raise NotImplementedError

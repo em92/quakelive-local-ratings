c759584451cf116c8c0591cd25c06e4abe19c621
Eugene Molotov <em92@users.noreply.github.com>
2019-03-15 17:52:08 +0500

    Finished writing logic for endpoint

--- endpoints.py
+++ endpoints.py
@@ -1,43 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from time import gmtime
-from email.utils import parsedate
+from email.utils import parsedate, formatdate
 from db import cache, get_db_pool
-from typing import Tuple, Optional
+from typing import Tuple
 
 from asyncpg import Connection
 from starlette.endpoints import HTTPEndpoint
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.responses import Response
-from starlette.types import Message, Receive, Scope, Send
 
 
 class Endpoint(HTTPEndpoint):
     last_req_time = None
+    last_mod_time = None
 
     def try_very_fast_response(self, request: Request):
-        last_req_time = self.last_req_time
+        if self.last_req_time is None:
+            return
 
-        if 'gametype' in request.path_params:
-            last_modified = cache.LAST_GAME_TIMESTAMPS[request.path_params["gametype"]]
-        else:
-            # TODO: move it to cache.LAST_GAME_TIMESTAMP
-            last_modified = max(cache.LAST_GAME_TIMESTAMPS.values())
-
+        last_modified = self.get_last_site_modified()
         last_modified = gmtime(last_modified)[:]
 
-        if last_req_time >= last_modified:
+        if self.last_req_time >= last_modified:
             raise HTTPException(304)
 
     async def try_fast_response(self, request: Request, conn: Connection) -> None:
-        last_req_time = self.last_req_time
+        if self.last_req_time is None:
+            return
 
-        last_modified = await self.get_last_modified(request, conn)
+        last_modified = await self.get_last_doc_modified(request, conn)
         if last_modified is None:
             return
 
-        if last_req_time >= last_modified:
+        if self.last_req_time >= last_modified:
             raise HTTPException(304)
 
     async def get(self, request: Request) -> Response:
@@ -47,30 +44,37 @@ class Endpoint(HTTPEndpoint):
             if request.path_params['gametype'] not in cache.GAMETYPE_IDS:
                 raise HTTPException(404, "invalid gametype: {}".format(request.path_params['gametype']))
 
-        last_req_time = parsedate(request.headers["if-modified-since"])
-        if last_req_time is not None:
-            self.last_req_time = last_req_time
+        self.last_req_time = parsedate(request.headers.get("if-modified-since"))
 
-            self.try_very_fast_response(request)
+        self.try_very_fast_response(request)
 
-            dbpool = await get_db_pool()
-            con = await dbpool.acquire()
-            with con.transaction():
-                await self.try_fast_response(request, con)
-                return await self.get_common_response(request, con)
+        dbpool = await get_db_pool()
+        con = await dbpool.acquire()
+        async with con.transaction():
+            await self.try_fast_response(request, con)
+            return await self.get_common_response(request, con)
+
+    def get_last_site_modified(self, request: Request):
+        if 'gametype' in request.path_params:
+            return cache.LAST_GAME_TIMESTAMPS[request.path_params["gametype"]]
         else:
-            dbpool = await get_db_pool()
-            con = await dbpool.acquire()
-            with con.transaction():
-                return await self.get_common_response(request, con)
+            # TODO: move it to cache.LAST_GAME_TIMESTAMP
+            return max(cache.LAST_GAME_TIMESTAMPS.values())
 
-    async def get_last_modified(self, request: Request, conn: Connection) -> Optional[Tuple]:
-        return None
+    async def get_last_doc_modified(self, request: Request, con: Connection) -> Tuple:
+        if self.last_mod_time:
+            return self.last_mod_time
 
-    async def get_common_response(self, request: Request, conn: Connection) -> Response:
-        resp = await self._get(request, conn)
-        # TODO: use get_last_modified
+        self.last_mod_time = await self._get_last_doc_modified(request, con)
+        return self.last_mod_time
+
+    async def get_common_response(self, request: Request, con: Connection) -> Response:
+        resp = await self._get(request, con)
+        resp.headers['Last-Modified'] = formatdate(await self._get_last_doc_modified(request, con), usegmt=True)
         return resp
 
-    async def _get(self, request: Request, conn: Connection) -> Response:
+    async def _get(self, request: Request, con: Connection) -> Response:
         raise NotImplementedError
+
+    async def _get_last_doc_modified(self, request: Request, con: Connection) -> Tuple:
+        return self.get_last_site_modified(request)

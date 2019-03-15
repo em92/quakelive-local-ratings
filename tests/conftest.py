00dffb71fb35b44abd3fa015ae419082198b985d
Eugene Molotov <em92@users.noreply.github.com>
2019-03-15 12:28:53 +0500

    Correctly handle database stoping in the end of pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_collection_modifyitems(items):
     # will execute as early as possible
     items.sort(key=lambda item: item.parent.obj.ORDER if hasattr(item.parent.obj, 'ORDER') else 0)
+
+
+def pytest_unconfigure(config):
+    from .fixture import postgresql
+    postgresql.stop()

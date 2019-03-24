0a3d4f356aa7cb861311c92f87d5f57a0f26c88f
Eugene Molotov <em92@users.noreply.github.com>
2019-03-24 18:22:53 +0500

    Using alternative method for RatingsJson and RatingsHtml

--- blueprints/ratings/__init__.py
+++ blueprints/ratings/__init__.py
@@ -6,48 +6,29 @@ from starlette.requests import Request
 from endpoints import Endpoint
 from .methods import get_list
 from templating import templates
+from asyncpg import Connection
 
-'''
-@app.route("/ratings/<gametype>/")
-@app.route("/ratings/<gametype>/<int:page>/")
-@try304
-def http_rating_gametype_page(gametype, page = 0):
-  show_inactive = request.args.get("show_inactive", False, type=bool)
-  return render_template("ratings_list.html", **rating.get_list( gametype, page, show_inactive ),
-    gametype = gametype,
-    current_page = page,
-    show_inactive = show_inactive,
-    page_suffix = ("?show_inactive=yes" if show_inactive else ""),
-    page_prefix = "/ratings/" + gametype
-  )
-
-
-@app.route("/ratings/<gametype>/<int:page>.json")
-@try304
-def http_ratings_gametype_page_json(gametype, page):
-  return jsonify( **rating.get_list( gametype, page ) )
-'''
 bp = App()
 
 
 @bp.route("/{gametype}/{page:int}.json")
 class RatingsJson(Endpoint):
-    async def get(self, request: Request):
+    async def _get(self, request: Request, con: Connection):
         page = request.path_params.get("page", 0)
-        gametype = request.path_params.get("gametype")
-        return JSONResponse(await get_list(gametype, page))
+        gametype_id = request.path_params.get("gametype_id")
+        return JSONResponse(await get_list(con, gametype_id, page))
 
 
-@bp.route("/{gametype}")
-@bp.route("/{gametype}/{page:int}")
+@bp.route("/{gametype}/")
+@bp.route("/{gametype}/{page:int}/")
 class RatingsHtml(Endpoint):
-    async def get(self, request: Request):
+    async def _get(self, request: Request, con: Connection):
         page = request.path_params.get("page", 0)
-        gametype = request.path_params.get("gametype")
+        gametype_id = request.path_params.get("gametype_id")
         show_inactive = request.query_params.get("show_inactive", False)
         if show_inactive:
             show_inactive = True
 
-        context = await get_list(gametype, page, show_inactive)
+        context = await get_list(con, gametype_id, page, show_inactive)
         context['request'] = request
         return templates.TemplateResponse("ratings_list.html", context)

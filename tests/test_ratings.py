21af75a77c8c192b7bef34f696272cc6c8900ba5
Eugene Molotov <em92@users.noreply.github.com>
2019-03-24 18:23:55 +0500

    Added test_ratings_ad_json

--- tests/test_ratings.py
+++ tests/test_ratings.py
@@ -12,3 +12,10 @@ class TestRatings(AppTestCase):
             resp.context['response'],
             self.read_json_sample("ratings_ad")
         )
+
+    def test_ratings_ad_json(self):
+        resp = self.test_cli.get("/ratings/ad/0.json")
+        self.assertEqual(
+            resp.json()['response'],
+            self.read_json_sample("ratings_ad")
+        )

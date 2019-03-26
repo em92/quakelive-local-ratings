6aa11cb1cf36aae2205df0e2f5ccfd9dabccbae3
Eugene Molotov <em92@users.noreply.github.com>
2019-03-26 17:50:55 +0500

    Filter out Undefined path_params in url_for jinja2 context function

--- qllr/templating.py
+++ qllr/templating.py
@@ -1,5 +1,6 @@
-from jinja2 import escape
+from jinja2 import escape, Undefined, contextfunction
 from starlette.templating import Jinja2Templates
+import typing
 
 from .db import cache
 
@@ -19,10 +20,17 @@ def seconds_to_mmss(value):
 
 class Templates(Jinja2Templates):
     def __init__(self, directory: str) -> None:
+        @contextfunction
+        def url_for(context: dict, name: str, **path_params: typing.Any) -> str:
+            request = context["request"]
+            path_params = {k: v for k, v in path_params.items() if not isinstance(v, Undefined)}
+            return request.url_for(name, **path_params)
+
         super().__init__(directory)
         self.env.filters['ql_nickname'] = render_ql_nickname
         self.env.filters['seconds_to_mmss'] = seconds_to_mmss
         self.env.globals['gametype_names'] = cache.GAMETYPE_NAMES
+        self.env.globals['url_for'] = url_for
 
 
 templates = Templates(directory='templates')

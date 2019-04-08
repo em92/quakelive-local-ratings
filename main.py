151d21a5254cd8f972427280ed732fb31842eea1
Eugene Molotov <em92@users.noreply.github.com>
2019-04-08 16:04:23 +0500

    Added trusted proxies

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import uvicorn
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
 
 from qllr import app
-from qllr.settings import HOST, PORT
+from qllr.settings import HOST, PORT, TRUSTED_PROXIES
+
+
+class TrustedProxiesMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
+        if "x-forwarded-for" in request.headers:
+            if request.client.host not in TRUSTED_PROXIES:
+                return PlainTextResponse("Client address spoofing detected", 403)
+        return await call_next(request)
+
+
+app.add_middleware(ProxyHeadersMiddleware)
+app.add_middleware(TrustedProxiesMiddleware)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=PORT, lifespan="on")

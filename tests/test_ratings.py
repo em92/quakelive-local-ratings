ad9648dbe1d4e25c41f88cb90ac6dc7814d29beb
Eugene Molotov <em92@users.noreply.github.com>
2019-07-28 19:57:22 +0500

    ratings: add missing show_inactive value in context

--- tests/test_ratings.py
+++ tests/test_ratings.py
@@ -1,8 +1,16 @@
+from pytest import mark, param
+
 from .conftest import read_json_sample
 
 
-def test_ratings_ad(service):
-    resp = service.get("/ratings/ad/")
+@mark.parametrize(
+    "query_string,expected_show_inactive",
+    [param("", False), param("show_inactive=yes", True)],
+)
+def test_ratings_ad(service, query_string: str, expected_show_inactive: bool):
+    uri = "/ratings/ad/"
+    uri += "?" + query_string if query_string else ""
+    resp = service.get(uri)
     context = resp.context
 
     assert "request" in context
@@ -12,6 +20,7 @@ def test_ratings_ad(service):
 
     assert context["gametype"] == "ad"
     assert context["current_page"] == 0
+    assert context["show_inactive"] == expected_show_inactive
 
     assert context["response"] == read_json_sample("ratings_ad")
 

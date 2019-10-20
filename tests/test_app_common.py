27faeea1af482f9cb491c059fabf5dfc241fb684
Eugene Molotov <em92@users.noreply.github.com>
2019-10-20 15:35:53 +0500

    convert_timestamp_to_tuple now always returns tuple

--- tests/test_app_common.py
+++ tests/test_app_common.py
@@ -1,7 +1,7 @@
 import requests
 from pytest import mark, raises
 
-from qllr.common import clean_name, request
+from qllr.common import clean_name, request, convert_timestamp_to_tuple as cttt
 
 
 @mark.asyncio
@@ -18,3 +18,8 @@ def test_clean_name():
     assert "MadGabZ" == clean_name("^1M^4ad^1G^4ab^1Z^7")
     assert "unnamed" == clean_name("")
     assert "unnamed" == clean_name("^0")
+
+
+def test_convert_timestamp_to_tuple():
+    assert cttt(None) == (1970, 1, 1, 0, 0, 0)
+    assert cttt(1571567579) == (2019, 10, 20, 10, 32, 59)

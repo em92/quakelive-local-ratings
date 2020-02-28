96a1195cb216fb9abdeb2d2cf93aedc0188794a0
Eugene Molotov <em92@users.noreply.github.com>
2020-02-28 18:00:18 +0500

    entrypoint: added simple fallback to get DATABASE_URL

--- docker/entrypoint.py
+++ docker/entrypoint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import socket
 import sys
 
@@ -7,7 +8,23 @@ from os import environ
 from time import sleep
 from urllib.parse import urlparse
 
-o = urlparse(environ["DATABASE_URL"])
+DATABASE_URL = None
+try:
+    DATABASE_URL = environ["DATABASE_URL"]
+except KeyError:
+    print("DATABASE_URL not defined as enviroment variable", file=sys.stderr)
+    envfile = os.path.dirname(os.path.realpath(__file__)) + "/../.env"
+    print("Trying to read from {}".format(envfile), file=sys.stderr)
+    with open(envfile) as f:
+        for line in f:
+            if line.startswith("DATABASE_URL"):
+                _, DATABASE_URL = line.strip().split('=', 1)
+
+if not DATABASE_URL:
+    print("DATABASE_URL not defined", file=sys.stderr)
+    sys.exit(1)
+
+o = urlparse(DATABASE_URL)
 host = o.hostname
 port = o.port or 5432
 

a3bdbeda708dec2ba5380acb7ca09716001857f9
Eugene Molotov <em92@users.noreply.github.com>
2020-12-23 03:51:04 +0500

    Use uvicorn's forwraded_allow_ips parameter instead of own TrustedProxiesMiddleware

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
 
 import uvicorn
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse
-from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from qllr import app
 from qllr.settings import HOST, PORT, TRUSTED_PROXIES
 
-
-class TrustedProxiesMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
-        if "x-forwarded-for" in request.headers:
-            if request.client.host not in TRUSTED_PROXIES:
-                return PlainTextResponse("Client address spoofing detected", 403)
-        return await call_next(request)
-
-
-if TRUSTED_PROXIES:
-    app.add_middleware(ProxyHeadersMiddleware)
-    app.add_middleware(TrustedProxiesMiddleware)
-
 if __name__ == "__main__":
-    uvicorn.run(app, host=HOST, port=PORT, lifespan="on")
+    uvicorn.run(app, host=HOST, port=PORT, lifespan="on", forwarded_allow_ips=TRUSTED_PROXIES)

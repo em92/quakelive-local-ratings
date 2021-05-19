3192be262743f4f7903686f8db00be644a843795
Eugene Molotov <em92@users.noreply.github.com>
2021-05-19 21:59:23 +0500

    lint main.py

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ from qllr import app
 from qllr.settings import HOST, PORT, TRUSTED_PROXIES
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=HOST, port=PORT, lifespan="on", forwarded_allow_ips=TRUSTED_PROXIES)
+    uvicorn.run(
+        app, host=HOST, port=PORT, lifespan="on", forwarded_allow_ips=TRUSTED_PROXIES
+    )

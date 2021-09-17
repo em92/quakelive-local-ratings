db6916d384702b26b8f470b09dfa6cfa475bb16f
Eugene Molotov <eugene.molotov@yandex.ru>
2021-09-18 00:41:33 +0500

    sql: added missing migration script for adding weapons

---
+++ sql/migrate3.sql
@@ -0,0 +1,6 @@
+INSERT INTO weapons (weapon_id, weapon_name, weapon_short) VALUES
+  (10, 'Grappling Hook',  'gh'),
+  (11, 'Nailgun',         'ng'),
+  (12, 'Prox Launcher',   'pm'),
+  (13, 'Chaingun',        'cg'),
+  (14, 'BFG',             'bfg');

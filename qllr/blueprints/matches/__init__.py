79c9d2de6849f8def73c2d675f8b28a5add8cf05
Eugene Molotov <eugene.molotov@yandex.ru>
2022-10-30 22:26:42 +0500

    matches: introducing top 25 best player matches per gametype

--- qllr/blueprints/matches/__init__.py
+++ qllr/blueprints/matches/__init__.py
@@ -8,7 +8,7 @@ from starlette.routing import Route
 from qllr.endpoints import Endpoint
 from qllr.templating import templates
 
-from .methods import get_last_matches
+from .methods import get_best_matches_of_player, get_last_matches
 
 
 class MatchesHtml(Endpoint):
@@ -25,6 +25,18 @@ class MatchesHtml(Endpoint):
         return templates.TemplateResponse("match_list.html", context)
 
 
+class BestMatchesHtml(Endpoint):
+    async def get_document(self, request: Request, con: Connection):
+        gametype = request.path_params["gametype"]
+        steam_id = request.path_params["steam_id"]
+
+        context = await get_best_matches_of_player(con, steam_id, gametype)
+        context["request"] = request
+        context["gametype"] = gametype
+        context["steam_id"] = steam_id
+        return templates.TemplateResponse("match_list.html", context)
+
+
 routes = [
     Route("/", endpoint=MatchesHtml),
     Route("/{page:int}/", endpoint=MatchesHtml),
@@ -32,6 +44,7 @@ routes = [
     Route("/player/{steam_id:int}/{page:int}/", endpoint=MatchesHtml),
     Route("/player/{steam_id:int}/{gametype}/", endpoint=MatchesHtml),
     Route("/player/{steam_id:int}/{gametype}/{page:int}/", endpoint=MatchesHtml),
+    Route("/player/{steam_id:int}/{gametype}/top", endpoint=BestMatchesHtml),
     Route("/{gametype}/", endpoint=MatchesHtml),
     Route("/{gametype}/{page:int}/", endpoint=MatchesHtml),
 ]
--- qllr/blueprints/matches/methods.py
+++ qllr/blueprints/matches/methods.py
@@ -10,6 +10,57 @@ from qllr.exceptions import InvalidGametype, PlayerNotFound
 MATCH_LIST_ITEM_COUNT = 25
 
 
+async def get_best_matches_of_player(con: Connection, steam_id: int, gametype: str):
+    try:
+        gametype_id = cache.GAMETYPE_IDS[gametype]
+    except KeyError:
+        raise InvalidGametype(gametype)
+
+    await con.set_type_codec(
+        "json", encoder=json.dumps, decoder=json.loads, schema="pg_catalog"
+    )
+
+    player_name = await _get_player_name(con, steam_id)
+
+    params = [
+        gametype_id,
+        steam_id,
+    ]
+
+    where_clause_str = """
+LEFT JOIN (
+    SELECT subs.match_id, match_perf
+    FROM scoreboards subs
+    LEFT JOIN matches subm ON subs.match_id = subm.match_id
+    WHERE subm.gametype_id = $1 AND
+    steam_id = $2 AND
+    match_perf IS NOT NULL AND
+    alive_time >= 1200
+    ORDER BY match_perf DESC
+    LIMIT {LIMIT}
+) s ON s.match_id = m.match_id
+WHERE s.match_id IS NOT NULL
+    """.format(
+        LIMIT=MATCH_LIST_ITEM_COUNT
+    )
+
+    matches = await _get_matches(
+        con,
+        where_clause_str,
+        params,
+        "match_perf DESC",
+        0,
+        MATCH_LIST_ITEM_COUNT,
+        "match_perf,",
+    )
+
+    return {
+        "page_count": 0,
+        "title": f"{player_name}'s best {cache.GAMETYPE_NAMES[gametype]} matches",
+        "matches": matches,
+    }
+
+
 async def get_last_matches(
     con: Connection, gametype=None, steam_id=None, page=0, from_ts=None, to_ts=None
 ):
@@ -60,13 +111,8 @@ async def get_last_matches(
         title = "Recent {} games".format(cache.GAMETYPE_NAMES[gametype])
 
     if steam_id:
-        row = await con.fetchval(
-            "SELECT name FROM players WHERE steam_id = $1", steam_id
-        )
-        if row is None:
-            raise PlayerNotFound(steam_id)
+        player_name = await _get_player_name(con, steam_id)
 
-        player_name = clean_name(row)
         title = "Recent games with {}".format(player_name) + (
             " (" + cache.GAMETYPE_NAMES[gametype] + ")" if gametype else ""
         )
@@ -101,6 +147,34 @@ async def get_last_matches(
         overall_match_count = await con.fetchval(query, *params)
         cache.store[cache_key] = overall_match_count
 
+    matches = await _get_matches(
+        con, where_clause_str, params, "timestamp DESC", offset, limit
+    )
+
+    return {
+        "page_count": ceil(overall_match_count / MATCH_LIST_ITEM_COUNT),
+        "title": title,
+        "matches": matches,
+    }
+
+
+async def _get_player_name(con: Connection, steam_id: int):
+    row = await con.fetchval("SELECT name FROM players WHERE steam_id = $1", steam_id)
+    if row is None:
+        raise PlayerNotFound(steam_id)
+
+    return clean_name(row)
+
+
+async def _get_matches(
+    con: Connection,
+    where_clause_str: str,
+    params,
+    order_by: str,
+    offset: int,
+    limit: int,
+    extra_rows=None,
+):
     # we assume, that offset and limit are ALWAYS integers
     query = """
     SELECT
@@ -112,9 +186,14 @@ async def get_last_matches(
             'team1_score', m.team1_score,
             'team2_score', m.team2_score,
             'map', mm.map_name
-        ) ORDER BY timestamp DESC), '{NOTHING}')
+        ) ORDER BY {ORDER_BY}), '{NOTHING}')
     FROM (
-        SELECT *
+        SELECT
+            m.gametype_id,
+            m.map_id,
+            team1_score, team2_score,
+            timestamp, {EXTRA_ROWS}
+            m.match_id
         FROM matches m
         {WHERE_CLAUSE}
         ORDER BY timestamp DESC
@@ -129,13 +208,8 @@ async def get_last_matches(
         NOTHING="{}",
         OFFSET=int(offset),
         LIMIT=int(limit),
+        ORDER_BY=order_by,
+        EXTRA_ROWS=extra_rows or "",
     )
 
-    row = await con.fetchval(query, *params)
-    matches = row
-
-    return {
-        "page_count": ceil(overall_match_count / MATCH_LIST_ITEM_COUNT),
-        "title": title,
-        "matches": matches,
-    }
+    return await con.fetchval(query, *params)

812c653e1fbb701647c5e36203013e841e543257
Eugene Molotov <eugene.molotov@yandex.ru>
2023-01-14 16:47:29 +0500

    tests: cover about page

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,6 +3,15 @@ def test_root_route(service):
     assert resp.headers["Location"].endswith("/matches/")
 
 
+def test_root_about_redirect(service):
+    resp = service.get("/about/", 307)
+    assert resp.headers["Location"].endswith("/about")
+
+
+def test_root_about(service):
+    service.get("/about", 200)
+
+
 def test_robots_txt(service):
     resp = service.get("/robots.txt", 200)
     assert "Allow:" not in resp.text
